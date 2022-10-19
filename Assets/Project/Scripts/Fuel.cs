7ac43bf17a223fec1a9674f20ba5a605e7657e70
apollinariia <apollinariia.orlova@gmail.com>
2022-10-19 21:21:47 +0300

    cleanup

---
+++ Assets/Project/Scripts/Fuel.cs
@@ -0,0 +1,21 @@
+﻿namespace Project
+{
+    public class Fuel
+    {
+        // todo property
+        public float curAmount;
+        public float maxAmount;
+        private float fullThresold = 1f;
+
+        public Fuel(float maxFuelAmount)
+        {
+            maxAmount = maxFuelAmount;
+            curAmount = maxFuelAmount;
+        }
+
+        public bool IsFull()
+        {
+            return (maxAmount - curAmount) < fullThresold;
+        }
+    }
+}
